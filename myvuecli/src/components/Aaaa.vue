15b15ff0fea29e059be43a20740a684dae53e3df
liulibin <liulibin@elensdata.com>
2020-03-11 14:11:04 +0800

    8

--- myvuecli/src/components/Aaaa.vue
+++ myvuecli/src/components/Aaaa.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="main">
-    
+    <div
+      :class="color()"
+      v-for="(item,index) of list"
+      :key="index"
+      v-on:click.prevent.self="handelClick(index)"
+      :style="{'color': index==id ? 'red':'green'}"
+    >{{item}}</div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      list: [1, 2, 3, 4],
+      id: "",
+      obj: {
+        age: 19,
+        name: "dd"
+      }
+    };
+  },
+  methods: {
+    color() {
+      console.log(7777);
+      return "color";
+    },
+    handelClick(index) {
+      console.log(index, 999);
+      this.id = index;
+      console.log(Object.is("1", "1"));
+      const { age } = this.obj;
+      console.log(age);
+    }
   }
 };
 </script>
--- myvuecli/vue.config.js
+++ myvuecli/vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   devServer: {
     port: 4201,
-    proxy: {
-      '/users': {
-        //要访问的跨域的域名
-        target: "http://127.0.0.1:3000",
-        // 是否启用websockets
-        ws: true,
-        //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样客户端端和服务端进行数据的交互就不会有跨域问题
-        changOrigin: true
-      }
-    }
+    // proxy: {
+    //   '/users': {
+    //     //要访问的跨域的域名
+    //     target: "http://127.0.0.1:3000",
+    //     // 是否启用websockets
+    //     ws: true,
+    //     //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样客户端端和服务端进行数据的交互就不会有跨域问题
+    //     changOrigin: true
+    //   }
+    // }
   },
   outputDir: './../nodeserver/public/assets'
 }
