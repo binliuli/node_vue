d150a7dbd2095884c194927ded1db9c87f23dbe0
liulibin <liulibin@elensdata.com>
2020-03-12 09:01:36 +0800

    eslint

---
+++ vue-cli-eslint/vue-eslint/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true, //此项是用来告诉eslint找当前配置文件不能往父级查找
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"//此项是用来指定eslint解析器的，解析器必须符合规则，babel-eslint解析器是对babel解析器的包装使其与ESLint解析
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-unused-vars": 1, // 不能有声明后未被使用的变量或参数
+  }
+};
